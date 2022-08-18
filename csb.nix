939988105bf824a1a16461e2d56b5df5e4478e16
David Madner <david.madner@gmail.com>
2022-08-18 07:14:25 +0200

    add nix file

---
+++ csb.nix
@@ -0,0 +1,16 @@
+with import <nixpkgs> {};
+
+stdenv.mkDerivation {
+    name = "node";
+    buildInputs = [
+        chromium
+        awscli2
+        nodejs
+    ];
+    systemPackages = [
+        nodePackages.pnpm
+    ]
+    shellHook = ''
+        export PATH="$PWD/node_modules/.bin/:$PATH"
+    '';
+}
